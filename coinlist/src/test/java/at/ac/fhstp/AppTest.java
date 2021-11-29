6817af149be2ea0ca368e7ecc417f1f1e553fa92
Is191303 <95129116+Is191303@users.noreply.github.com>
2021-11-29 23:50:04 +0100

    AppTest

--- coinlist/src/test/java/at/ac/fhstp/AppTest.java
+++ coinlist/src/test/java/at/ac/fhstp/AppTest.java
@@ -1,20 +1,35 @@
 package at.ac.fhstp;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-) @is191310
-     */
+public class AppTest {
+
+    // simple request - response
+
+ 
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void givenRequestWithNoAcceptHeader_whenRequestIsExecuted_thenDefaultResponseContentTypeIsJson() throws ClientProtocolException, IOException {
+        // Given
+        final String jsonMimeType = "application/json";
+        final HttpUriRequest request = new HttpGet("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?cryptocurrency_type=tokens&sort=");
+
+        // When
+        final HttpResponse response = HttpClientBuilder.create().build().execute(request);
+
+        // Then
+        final String mimeType = ContentType.getOrDefault(response.getEntity()).getMimeType();
+        assertEquals(jsonMimeType, mimeType);
     }
+
 }
