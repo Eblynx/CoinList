62b52689b7b6fe2016a539ab057ecda776c471a3
Christian Hierzer <is191310@fhstp.ac.at>
2021-11-30 22:50:59 +0100

    update

--- coinlist/src/main/java/at/ac/fhstp/App.java
+++ coinlist/src/main/java/at/ac/fhstp/App.java
@@ -1,17 +1,9 @@
 package at.ac.fhstp;
 
 import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
 import java.util.stream.IntStream;
-
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
-
-
 
 class App {
 
@@ -27,59 +19,33 @@ class App {
         System.out.println("=====Kurswert[EUR]-Top"+limit+"=====");      
         printlist(jarray, price, waehrung);
         
-        jarray = sortTopMover(jarray, percent_change_24h, waehrung);
         System.out.println("=====TopMover[%]-Top"+limit+"=====");
-        printlist(jarray, percent_change_24h, waehrung);
+        filter(jarray, percent_change_24h, waehrung);
+ 
           
     }
 
     public static void printlist(JSONArray jarray, String type, String waehrung) throws URISyntaxException
     {       
+        for(int i=0; i < jarray.length(); ++i)
+        {
+            System.out.print("Token: "+jarray.getJSONObject(i).getString("name")+" | "+type+": ");
+            System.out.println(jarray.getJSONObject(i).getJSONObject("quote").getJSONObject(waehrung).getDouble(type));
+        }
+    }
+    
+    public static void filter(JSONArray jarray, String type, String waehrung) throws URISyntaxException
+    {       
         IntStream.range(0, jarray.length())
         .mapToObj(i -> (JSONObject) jarray.getJSONObject(i))
+        .sorted((i,j)-> Double.compare(j.getJSONObject("quote").getJSONObject(waehrung).getDouble(type), i.getJSONObject("quote").getJSONObject(waehrung).getDouble(type)))
         .forEach(i -> 
         {
             System.out.print("Token: "+i.getString("name")+" | "+type+": ");
             System.out.println(i.getJSONObject("quote").getJSONObject(waehrung).getDouble(type));
         });
-        
-        /*
-        for(int i=0; i < list.length(); ++i)
-        {
-            System.out.print("Token: "+list.getJSONObject(i).getString("name")+" | "+type+": ");
-            System.out.println(list.getJSONObject(i).getJSONObject("quote").getJSONObject(waehrung).getDouble(type));
-        }*/
     }
-    public static void filter(JSONArray jarray, String type, String waehrung) throws URISyntaxException
-    {       
-        IntStream.range(0, jarray.length())
-        .mapToObj(i -> (JSONObject) jarray.getJSONObject(i))
-        .sorted(Comparator.comparingDouble(keyExtractor))
-        .forEach(i -> System.out.println(i.getJSONObject("quote").getJSONObject(waehrung).getDouble(type)));
-        
-        /*
-        for(int i=0; i < list.length(); ++i)
-        {
-            System.out.print("Token: "+list.getJSONObject(i).getString("name")+" | "+type+": ");
-            System.out.println(list.getJSONObject(i).getJSONObject("quote").getJSONObject(waehrung).getDouble(type));
-        }
-    }*/
-
-
-    public static JSONArray sortTopMover(JSONArray jarray, String type, String waehrung) throws JSONException {
-        List<JSONObject> list = new ArrayList<JSONObject>();
-        for (int i = 0; i < jarray.length(); i++) 
-        {
-                list.add(jarray.getJSONObject(i));
-        }
-        Collections.sort(list, new SortingToken(type, waehrung));
-
-        JSONArray resultArray = new JSONArray(list);
-
-        return resultArray;
-    }
-
-    
+       
 }
 
 
