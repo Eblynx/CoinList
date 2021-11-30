582fe49f692f8253a5af26e4ef2b460443729cf5
Christian Hierzer <is191310@fhstp.ac.at>
2021-11-30 19:28:17 +0100

    add topmover sort with comperator

---
+++ coinlist/src/main/java/at/ac/fhstp/SortingCoin.java
@@ -0,0 +1,33 @@
+package at.ac.fhstp;
+
+import java.util.Comparator;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class SortingCoin implements Comparator<JSONObject> {
+
+    
+    private String type;
+    private String waehrung;
+
+    public SortingCoin(String type, String waehrung) {
+        this.type = type;
+        this.waehrung = waehrung;
+    }
+    
+    
+    @Override
+    public int compare(JSONObject array1, JSONObject array2) {
+        double d1 = array1.getJSONObject("quote").getJSONObject(waehrung).getDouble(type);
+        double d2 = array2.getJSONObject("quote").getJSONObject(waehrung).getDouble(type);
+        
+        try {
+            return Double.compare(d2, d1);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+       return 0;
+
+    }   
+}
