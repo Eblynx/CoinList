6d51960814fe7c98eb7266559a2e25ca46bf6c11
Christian Hierzer <is191310@fhstp.ac.at>
2021-11-30 19:27:56 +0100

    update filter topmover

--- coinlist/src/main/java/at/ac/fhstp/App.java
+++ coinlist/src/main/java/at/ac/fhstp/App.java
@@ -1,8 +1,13 @@
 package at.ac.fhstp;
 
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 class App {
@@ -10,21 +15,35 @@ class App {
     public static void main(String[] args) throws URISyntaxException
     {       
         System.out.println("=====Kurswert[EUR]-Top5=====");
-        printlist("price", "EUR", 5);
+        
+        JSONArray list = HttpClientCoin.httpStart("price", "EUR", 5);
+        printlist(list, "price", "EUR");
+        JSONArray nlist = sortTopMover(list, "percent_change_24h", "EUR");
         System.out.println("=====TopMover[%]-Top5=====");
-        printlist("percent_change_24h", "EUR", 5);
+        printlist(nlist, "percent_change_24h", "EUR");
           
     }
 
-    public static void printlist(String type, String waehrung, int limit) throws URISyntaxException
+    public static void printlist(JSONArray list, String type, String waehrung) throws URISyntaxException
     {
-        
-        JSONArray list = HttpClientCoin.httpStart(type, waehrung, limit);
-        for(int i=0; i < limit; ++i)
+        for(int i=0; i < list.length(); ++i)
         {
             System.out.print("Coin: "+list.getJSONObject(i).getString("name")+" | "+type+": ");
             System.out.println(list.getJSONObject(i).getJSONObject("quote").getJSONObject(waehrung).getDouble(type));
         }
+    }
+
+    public static JSONArray sortTopMover(JSONArray array, String type, String waehrung) throws JSONException {
+        List<JSONObject> list = new ArrayList<JSONObject>();
+        for (int i = 0; i < array.length(); i++) 
+        {
+                list.add(array.getJSONObject(i));
+        }
+        Collections.sort(list, new SortingCoin(type, waehrung));
+
+        JSONArray resultArray = new JSONArray(list);
+
+        return resultArray;
     }
     
     
