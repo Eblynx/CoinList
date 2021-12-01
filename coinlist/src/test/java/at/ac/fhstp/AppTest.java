575f358a7f9a0fcd5e8d0dbfff1a170fa9236c4a
Is191303 <95129116+Is191303@users.noreply.github.com>
2021-12-01 10:42:19 +0100

    Test Exceptions

--- coinlist/src/test/java/at/ac/fhstp/AppTest.java
+++ coinlist/src/test/java/at/ac/fhstp/AppTest.java
@@ -2,10 +2,15 @@ package at.ac.fhstp;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.http.HttpResponse;
 import org.apache.http.HttpStatus;
+import org.json.HTTP;
+
 import org.junit.jupiter.api.Test;
 
 public class AppTest 
@@ -18,5 +23,27 @@ public class AppTest
         System.out.println("API und API-Key funktionieren korrekt");
     }
 
+    @Test
+public void ExceptionThrown() {
+    Exception exception = assertThrows(NumberFormatException.class, () -> {
+        Integer.parseInt("1a");
+    });
+    String expectedMessage = "For input string";
+    String actualMessage = exception.getMessage();
 
+    assertTrue(actualMessage.contains(expectedMessage));
+    System.out.println(expectedMessage);
+} 
+@Test
+void exceptionTesting() {
+    String httpStr = null;
+    NullPointerException thrown = assertThrows(
+        NullPointerException.class,
+           () -> HTTP.toJSONObject(httpStr),
+           "Expected doThing() to throw, but it didn't"
+    );
+    System.out.println("Inhalt der JSON Datei ist "+ httpStr);
+
+    assertTrue(thrown.getMessage().contains("null"));
+} 
 }
